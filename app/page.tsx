147911f5deee08d864fc6d37632958161de2adce
MarcusL <aibekkuna@gmail.com>
2026-07-31 19:24:54 +0600

    aaaa

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
+import Loader from "../components/Loader/Loader";
+
 export default function Home() {
   return (
-    <main
-      style={{
-        minHeight: "100vh",
-        background: "#FAF9F7",
-        color: "#5F554B",
-        padding: "40px",
-        fontSize: "30px",
-      }}
-    >
-      МОБИЛЬНЫЙ ТЕСТ
-
-      <p>Если ты видишь этот текст — Vercel и Next.js работают.</p>
+    <main>
+      <Loader />
     </main>
   );
 }
