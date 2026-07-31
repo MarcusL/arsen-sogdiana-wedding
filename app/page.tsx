2cae595a6c797f4987eb27b9ed7919891cb2c313
MarcusL <aibekkuna@gmail.com>
2026-07-31 19:48:20 +0600

    testtest3

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Loader from "../components/Loader/Loader";
+import Petals from "../components/Petals/Petals";
+import Welcome from "../components/Welcome/Welcome";
+import Countdown from "../components/Countdown/Countdown";
 
 export default function Home() {
   return (
     <main>
+      <Petals />
+
       <Loader />
+
+      <Welcome />
+
+      <Countdown />
     </main>
   );
 }
